77c95aff4c672303158afdeec5df8bba83817ade
Mitanshu-Singhal <116966995+Mitanshu-Singhal@users.noreply.github.com>
2023-07-31 18:35:35 +0530

    Add files via upload

---
+++ W3_Day_16/Program_1.cpp
@@ -0,0 +1,51 @@
+class Queue {
+
+ 
+
+    int front, rear;
+
+    vector<int> arr;
+
+ 
+
+public:
+
+    Queue()
+
+    {
+
+        front = 0;
+
+        rear = -1;
+
+        arr.resize(100001);
+
+    }
+
+ 
+
+    // Enqueue (add) element 'e' at the end of the queue.
+
+    void enqueue(int e)
+
+    {
+
+        arr[++rear] = e;
+
+    }
+
+ 
+
+    // Dequeue (retrieve) the element from the front of the queue.
+
+    int dequeue()
+
+    {
+
+        if(front > rear) {front = 0; rear = -1; return -1;}
+
+        else return arr[front++];
+
+    }
+
+};
